7fc405ccfc3d6d74133cc2af826f5bcca34b7cfe
SpyrosGeo <xarodasimba@gmail.com>
2025-04-22 22:20:53 +0300

    added basic model

---
+++ model.go
@@ -0,0 +1,39 @@
+package main
+
+import "time"
+type Mode int
+
+const (
+	// Node types
+	Pomodoro Mode = iota
+	ShortBreak
+	LongBreak
+)
+func (m Mode) String() string {
+    switch m {
+    case Pomodoro:
+        return "Pomodoro"
+    case ShortBreak:
+        return "Short Break"
+    case LongBreak:
+        return "Long Break"
+    default:
+        return "Unknown"
+    }
+}
+// Model holds the application state
+type Model struct {
+    Mode          Mode          // Current session type
+    TimeLeft      time.Duration // Time remaining
+    PomodoroCount int           // Number of completed Pomodoros
+    Running       bool          // Is the timer active?
+}
+
+
+func initialModel() Model {
+return Model{
+	Mode:          Pomodoro,
+	TimeLeft:      25 * time.Minute,
+	PomodoroCount: 0,
+	Running:       false,}
+}
